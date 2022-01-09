c4c01eaca38c2a1249f67365ebcbae1e811bb8ac
Chirilov Narcis <chirilov.narcis@yahoo.ro>
2022-01-09 12:54:13 +0200

    changed api

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: process.env.NODE_ENV == "development" ? 'http://localhost:5000/api' : "https://blog.chirilovnarcis.ro/api",
      headers: {
       'Content-Type': 'application/json',
      },
